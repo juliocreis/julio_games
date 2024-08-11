a5c480bb5c7b5e2116462bb58e4dc9505729a23e
Julio Cesar <julioreis951@gmail.com>
2024-08-11 19:24:30 -0300

    exibir mensagem de confirmação

--- js/app.js
+++ js/app.js
@@ -1,16 +1,44 @@
+let jogosAlugados = 0;
+
+
+function quantidadeJogosAlugados(){
+    console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(gameId) {
     let jogoClicado = document.getElementById(`game-${gameId}`);
     let imagem = jogoClicado.querySelector('.dashboard__item__img');
     let botao = jogoClicado.querySelector('.dashboard__item__button');
+    let mensagemConfirmacao;
 
-    if(imagem.classList.contains('dashboard__item__img--rented')){
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+    if(imagem.classList.contains('dashboard__item__img--rented')) {
+        mensagemConfirmacao = 'Você realmente deseja devolver este jogo?';
     }
     else {
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.textContent = 'Devolver';
-        botao.classList.add('dashboard__item__button--return');
+        mensagemConfirmacao = 'Você realmente deseja alugar este jogo?';
     }
+
+    let confirmar = confirm(mensagemConfirmacao);
+
+    if(confirmar){
+        if(imagem.classList.contains('dashboard__item__img--rented')){
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+            jogosAlugados--;
+        }
+        else {
+            imagem.classList.add('dashboard__item__img--rented');
+            botao.textContent = 'Devolver';
+            botao.classList.add('dashboard__item__button--return');
+            jogosAlugados++;
+        }
+    }
+
+    quantidadeJogosAlugados();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    contarEExibirJogosAlugados();
+});
