5f82c6ef03a1de96a19e9b1adb2b4b5639662474
Julio Cesar <julioreis951@gmail.com>
2024-08-11 18:56:15 -0300

    criando função para alterar o status dos botões

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(gameId) {
+    let jogoClicado = document.getElementById(`game-${gameId}`);
+    let imagem = jogoClicado.querySelector('.dashboard__item__img');
+    let botao = jogoClicado.querySelector('.dashboard__item__button');
+
+    if(imagem.classList.contains('dashboard__item__img--rented')){
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    }
+    else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+    }
+}
